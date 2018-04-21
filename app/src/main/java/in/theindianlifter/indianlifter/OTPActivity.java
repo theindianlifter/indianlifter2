941e67ce16ab96490bfb5151ce9f7929439716e0
Rajat Dhamija <rajat.dhamija@mail.click-labs.com>
2018-04-21 14:57:43 +0530

    setup firebase

--- app/src/main/java/in/theindianlifter/indianlifter/OTPActivity.java
+++ app/src/main/java/in/theindianlifter/indianlifter/OTPActivity.java
@@ -3,11 +3,31 @@ package in.theindianlifter.indianlifter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class OTPActivity extends AppCompatActivity {
+    private String otp = "4444";
+    private DatabaseReference mainRef;
+    private DatabaseReference authRef;
+    private FirebaseDatabase myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+        setupFirebase();
+        savePhoneNumberToDatabase();
+    }
+
+    private void setupFirebase() {
+        myDb = FirebaseDatabase.getInstance();
+        mainRef = myDb.getReference("contact_numbers");
+    }
+
+    private void savePhoneNumberToDatabase() {
+        authRef = mainRef.push();
+        authRef.child("contact_number").setValue("9872226052");
+        authRef.child("country_code").setValue("+91");
     }
 }
--- app/src/main/java/in/theindianlifter/indianlifter/SignupActivity.java
+++ app/src/main/java/in/theindianlifter/indianlifter/SignupActivity.java
@@ -2,12 +2,65 @@ package in.theindianlifter.indianlifter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = SignupActivity.class.getSimpleName();
+    private String contactNumber = "+919872226052";
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        setCallbacks();
+        sendOTP();
     }
+
+    private void setCallbacks() {
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+                Log.d(TAG, "onVerificationCompleted:" + credential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Log.w(TAG, "onVerificationFailed", e);
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                    // ...
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                    // ...
+                }
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+                Log.d(TAG, "onCodeSent:" + verificationId);
+            }
+        };
+    }
+
+    private void sendOTP() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                contactNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
 }
